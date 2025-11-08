7fc8b5844f3fdd02f15fcbf67366d5e0e45202d0
nener0o <159264333+nener0o@users.noreply.github.com>
2025-11-08 18:54:15 +0300

    commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getProfile, logout } from '../services/authService';
+import { getProfile, logout, getUsernameFromToken } from '../services/authService';
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
